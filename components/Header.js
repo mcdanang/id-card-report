8a2cdc06f67ebffc95e0fdc90dd4e0734e089bff
Danang Priambodo <mdanangpriambodo@gmail.com>
2024-08-16 10:25:16 +0700

    add mobile responsive

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header className="bg-slate-600 text-white">
       <div className="container mx-auto px-4 py-6">
-        <div className="flex justify-between items-center">
-          <div>
+        <div className="flex flex-col sm:flex-row gap-4 justify-between items-center">
+          <div className="text-center sm:text-left">
             <h1 className="text-2xl font-bold">Lapor Penyalahgunaan KTP</h1>
             <p className="text-sm mt-1">Pemilihan Gubernur Jakarta</p>
           </div>
