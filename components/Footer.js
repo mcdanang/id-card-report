543fbe6efff0c2b2b79176743be3e992aff78790
Danang Priambodo <mdanangpriambodo@gmail.com>
2024-08-16 10:25:26 +0700

    add github link

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 text-gray-600">
+    <footer className="bg-gray-100 text-slate-400">
       <div className="container mx-auto px-4 py-6">
         <div className="text-center">
-          <p>
+          <p className="text-sm">
             &copy; {new Date().getFullYear()} Lapor Penyalahgunaan KTP. Hak
             Cipta Dilindungi.
           </p>
@@ -11,6 +11,17 @@ export default function Footer() {
             Situs ini dibuat untuk melaporkan penggunaan KTP yang tidak sah
             dalam pemilihan Gubernur Jakarta. Kami berkomitmen untuk menjaga
             kerahasiaan data Anda.
+          </p>
+          <p className="mt-2 text-sm">
+            Bantu kontribusi di proyek open source{" "}
+            <a
+              href="https://github.com/mcdanang/id-card-report"
+              target="_blank"
+              className="text-blue-400"
+            >
+              Github
+            </a>
+            .
           </p>
         </div>
       </div>
